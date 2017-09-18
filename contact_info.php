6fd44d1898ba07a89283e3611bd24f68a3af2c36
valdeslab <dvjr22@gmail.com>
2017-09-18 13:55:31 -0400

    contact div changes

---
+++ contact_info.php
@@ -0,0 +1,19 @@
+<?php
+
+
+if(isset($_POST['name']) && isset($_POST['email'])) {
+    $data = $_POST['name'] . '-' . $_POST['email'] . "\n";
+    $ret = file_put_contents('Contact/mydata.txt', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "$ret bytes written to file";
+    }
+}
+else {
+   die('no post data to process');
+}
+
+?>
+
